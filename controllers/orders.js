0d35fb4160dd9515f5d914e601e061a762ce22f5
Fernando Guimaraes <ferguimara@gmail.com>
2021-06-05 16:24:13 -0400

    full crud complete

---
+++ controllers/orders.js
@@ -0,0 +1,34 @@
+var Order = require('../models/order')
+
+module.exports = {
+    index,
+    create,
+    update,
+    delete: deleteOrder,
+};
+
+function index(req, res) {
+    Order.find({}, function(err, skills) {
+      res.status(200).json(orders);
+    });
+}
+
+function create(req, res) {
+    Order.create(req.body, function(err, order) {
+      res.status(201).json(order);
+    });
+}
+
+function update(req, res) {
+    Order.findByIdAndUpdate(req.params.id, req.body, function () {
+        //calling index to forward the request to our index action so we can send a brand new list of orders as a response to updating an order
+        index(req, res);
+    });
+}
+
+function deleteOrder(req, res) {
+    Order.findByIdAndDelete(req.params.id, function () {
+        index(req, res);
+    })
+}
+
--- routes/orders.js
+++ routes/orders.js
@@ -1,23 +1,22 @@
 var express = require('express');
 const order = require('../models/order');
 var router = express.Router();
-//Require our model
-var Order = require('../models/order')
+//Require our controller
+var ordersCtrl = require('../controllers/orders');
+
 
 //Routes:
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.status(200).json([
-    {date: '6/5/2021', product: 'iMac', price: '$2000', status: 'For Sale'}
-  ])
-});
+router.get('/', ordersCtrl.index);
 
-/* Create Route w/ controller logic combined*/
-router.post('/', function(req, res) {
-  Order.create(req.body, function(err, order) {
-    res.status(201).json(order);
-  });
-});
+/* Create Route */
+router.post('/', ordersCtrl.create);
+
+/* Update Route */
+router.put('/:id', ordersCtrl.update);
+
+/* Delete User */
+router.delete('/:id', ordersCtrl.delete);
 
 module.exports = router;
